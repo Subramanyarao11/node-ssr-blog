6d8539c035b20b0baeb6713485106db9f94c4e5a
Subramanyarao11 <subramanya11rao@gmail.com>
2022-10-25 23:53:45 +0530

    template

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
-const path = require("path");
-app.use('/dist',express.static('dist'));
 
+app.use('/dist',express.static('dist'));
+app.use(bodyParser.urlencoded( {extended: true} ));
 
 app.get("/" , (req , res)=>{
     res.sendFile(path.join(__dirname , "src" ,"index.html"));
@@ -11,6 +13,14 @@ app.get("/" , (req , res)=>{
 
 app.get("/contact" , (req , res)=>{
     res.sendFile(path.join(__dirname , "src" ,"contact.html"));
+})
+
+app.post("/contact" , (req,res) => {
+    // console.log(req.body);
+    const name = req.body.name;
+    const email = req.body.email;
+    const msg = req.body.message;
+    console.log(name , email , msg);
 })
 
 app.get("/blog" , (req , res)=>{
@@ -22,3 +32,6 @@ const port = process.env.VARIABLE_NAME || 3000
 app.listen(port , () => {
     console.log(`Server is running on port: ${port}` );
 })
+
+// API Key
+// ef5fa4b72a41c354d1706ffda2ba9129-us21
